e3c07e718384d22c5eebd56974c62faa8bddbf77
kevindurston21 <43143552+kevindurston21@users.noreply.github.com>
2021-05-14 22:10:02 +0100

    Setting up to release 1.1.0 - docker and script edits
    Edit docker file for debian prd image to include 2 environment variables required to allow the alive-progress bars to work.
    
    Edit docker file for debian dev image build to use an unedited requirements.txt file - previously had been removing -e for setup.py and running later for an efficient image, but this required manual editing.  Script now refreshes requirements.txt and uses the new file in the build, slower but more reliable approach
    
    Edit docker file to accept a --build-arg which passes in the name of the tar file form the build script.  This simplifies version updates.
    
    Add .dockerignore file to dev docker file folder and have script copy it in for the build and remove it after
    
    Update requirements.txt
    
    update scripts for version 1.1.0
    update config.py for v 1.1.0

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -8,27 +8,37 @@
 # Second argument is a docker build number this is a positive value or if left blank it will use version `0`
 # NOTE if also using `push` it will NOT push a number tag only a latest tag to docker hub
 APP_NAME="yanom"
-APP_TAR="yanom1.0.0-debian10-slim-buster.tar.gz"
+VERSION="1.1.0"
+APP_TAR="yanom-$VERSION-debian10-slim-buster.tar.gz"
 DOCKER_REPO="thehistorianandthegeek"
 DEV_IMAGE="yanom-dev-deb10"
-DEV_DOCKERFILE_PATH="dockerfiles/yanom-dev-deb10-slim-buster/Dockerfile"
-PROD_DOCKERFILE_PATH="dockerfiles/yanom-prod-deb10-slim-buster/Dockerfile"
+DEV_DOCKERFILE_PATH="dockerfiles/yanom-dev-deb10-slim-buster"
+PROD_DOCKERFILE_PATH="dockerfiles/yanom-prod-deb10-slim-buster"
 PROD_DIST_PATH="dist"
 BUILD_NUMBER=0
 if [ "$2" ] && [ "$2" -gt 0 ]
 then
   BUILD_NUMBER=$2
 fi
+# refresh requirements.txt  this adds time to build but helps when forget to update requirements!
+echo updating requirements.txt
+pipenv lock --requirements > requirements.txt
+
+# copy in required dockerignore file
+cp $DEV_DOCKERFILE_PATH/.dockerignore .dockerignore
+
 # Build docker image to create single file package
 docker ps -aq --filter "name=$DEV_IMAGE" | grep -q . && docker stop $DEV_IMAGE && docker rm -fv $DEV_IMAGE
-docker build -t $DEV_IMAGE:latest -f $DEV_DOCKERFILE_PATH .
+docker build --build-arg APP_TAR=$APP_TAR -t $DEV_IMAGE:latest -f $DEV_DOCKERFILE_PATH/Dockerfile .
 docker container create --name $DEV_IMAGE $DEV_IMAGE:latest
 docker cp $DEV_IMAGE:/app/$APP_TAR $PROD_DIST_PATH
 docker rm -f $DEV_IMAGE
 docker image rm $DEV_IMAGE:latest
 
+## remove the docker ignore file
+rm .dockerignore
 ## Build and push docker production image
-docker build -t $DOCKER_REPO/$APP_NAME:"$BUILD_NUMBER" -t $DOCKER_REPO/$APP_NAME:latest -f $PROD_DOCKERFILE_PATH .
+docker build --build-arg APP_TAR=$APP_TAR -t $DOCKER_REPO/$APP_NAME:"$BUILD_NUMBER" -t $DOCKER_REPO/$APP_NAME:latest -f $PROD_DOCKERFILE_PATH/Dockerfile .
 if [ "$1" ] && [ "$1" == "push" ]
 then
   docker push $DOCKER_REPO/$APP_NAME:latest
--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 APP_NAME = 'YANOM'
 APP_SUB_NAME = 'Note-O-Matic'
-VERSION = 1.0
+VERSION = '1.1.0'
 DATA_DIR = 'data'
 global logger_level
 logger_level = 20  # INFO
